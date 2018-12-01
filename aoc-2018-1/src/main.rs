eea2d0fb5fe4b4f1260e1e60681569148a89eaea
Michael Maurizi <mmaurizi@azavea.com>
2018-12-01 10:00:47 -0500

    Clean-up

--- aoc-2018-1/src/main.rs
+++ aoc-2018-1/src/main.rs
@@ -1,26 +1,28 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
 fn part1(input: &str) -> i32 {
     input.lines().map(|c| c.parse::<i32>().unwrap()).sum()
 }
 
-fn part2(input: &str) -> i32 {
-    let nums: Vec<i32> = input.lines().map(|c| c.parse().unwrap()).collect();
+fn part2(input: &str) -> Option<i32> {
     let mut freq = 0;
-    let mut i = 0;
-    let mut frequencies = vec!(freq);
-    loop {
-        freq += nums[i];
+    let mut frequencies: HashSet<i32> = HashSet::from_iter([freq].iter().cloned());
+    for chr in input.lines().cycle() {
+        let num = chr.parse::<i32>().ok()?;
+        freq += num;
         if frequencies.contains(&freq) {
-            return freq;
+            return Some(freq);
         }
-        frequencies.push(freq);
-        i = (i + 1) % nums.len();
+        frequencies.insert(freq);
     }
+    None
 }
 
 fn main() {
     let input = include_str!("input.txt").trim();
     println!("{}", part1(input));
-    println!("{}", part2(input));
+    println!("{}", part2(input).unwrap());
 }
 
 #[test]
@@ -32,8 +34,8 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2("+1\n-1\n"), 0);
-    assert_eq!(part2("+3\n+3\n+4\n-2\n-4\n"), 10);
-    assert_eq!(part2("-6\n+3\n+8\n+5\n-6\n"), 5);
-    assert_eq!(part2("+7\n+7\n-2\n-7\n-4\n"), 14);
+    assert_eq!(part2("+1\n-1\n").unwrap(), 0);
+    assert_eq!(part2("+3\n+3\n+4\n-2\n-4\n").unwrap(), 10);
+    assert_eq!(part2("-6\n+3\n+8\n+5\n-6\n").unwrap(), 5);
+    assert_eq!(part2("+7\n+7\n-2\n-7\n-4\n").unwrap(), 14);
 }
