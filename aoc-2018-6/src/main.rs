0992672203a312dd1b6973b9e8dd53d90820763a
Michael Maurizi <mmaurizi@azavea.com>
2018-12-06 22:59:00 -0500

    2018 Day 6 part 1 done, part 2 in progress

--- aoc-2018-6/src/main.rs
+++ aoc-2018-6/src/main.rs
@@ -1,6 +1,7 @@
 extern crate itertools;
 
 use itertools::Itertools;
+use std::collections::HashSet;
 
 static GRID_SIZE: usize = 500;
 
@@ -23,26 +24,22 @@ fn part1(input: &str) -> Option<usize> {
                 .map(|(index, _)| index);
         }
     }
-    grid
-        .into_iter()
-        .enumerate()
-        .flat_map(|(row_idx, row)| {
-            row
-                .into_iter()
-                .filter_map(|x| x)
-                .enumerate()
-                .map(|(col_idx, cell): (usize, usize)| {
-                    let row_copy = row_idx.clone();
-                    (row_copy, col_idx, cell)
-                })
-        })
-        .filter_map(|(row, col, cell)| {
-            if col != 0 && col != GRID_SIZE - 1 && row != 0 && row != GRID_SIZE - 1 {
-                Some(cell)
-            } else {
-                None
+
+
+    let mut infinite = HashSet::new();
+    for i in 0..GRID_SIZE {
+        let points = [(0, i), (i, 0), (GRID_SIZE - 1, i), (i, GRID_SIZE - 1)];
+        for (x, y) in points.iter() {
+            if let Some(cell) = grid[*x][*y] {
+                infinite.insert(cell);
             }
-        })
+        }
+    }
+    grid
+        .iter()
+        .flat_map(|f| f.iter())
+        .filter_map(|&x| x)
+        .filter(|index| !infinite.contains(index))
         .sorted()
         .into_iter()
         .group_by(|cell: &usize| *cell)
@@ -51,17 +48,52 @@ fn part1(input: &str) -> Option<usize> {
         .max()
 }
 
+fn part2(input: &str, threshold: i64) -> Option<usize> {
+    let mut grid: Vec<Vec<i64>> = vec![vec![0; GRID_SIZE]; GRID_SIZE];
+    let points: Vec<(usize, usize)> = input.lines().map(|line| {
+        let nums: Vec<&str> = line.split(", ").collect();
+        (nums[0].parse().unwrap(), nums[1].parse().unwrap())
+    }).collect();
+
+    for i in 0..GRID_SIZE {
+        for j in 0..GRID_SIZE {
+            grid[i][j] = points
+                .iter()
+                .enumerate()
+                .map(|(_, &point)| {
+                    let (x, y) = point;
+                    (i as i64 - x as i64).abs() + (j as i64 - y as i64).abs()
+                })
+                .sum();
+        }
+    }
+
+    Some(grid.iter().flat_map(|f| f.iter()).filter(|cell| **cell > threshold).count())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+    println!("{}", part1(input).unwrap());
 }
 
 #[test]
 fn test_part1() {
     let input = "1, 1
 1, 6
-8,3
+8, 3
 3, 4
-5,5
+5, 5
 8, 9";
     assert_eq!(part1(input), Some(17));
+}
+
+#[test]
+fn test_part2() {
+    let input = "1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9";
+    assert_eq!(part2(input, 32), Some(16));
 }
