7235821832a027a1b5743dacaab6315d62e4a08d
Michael Maurizi <mmaurizi@azavea.com>
2018-12-08 19:09:49 -0500

    2018 Day 7 part 1 work in progress

--- aoc-2018-7/src/main.rs
+++ aoc-2018-7/src/main.rs
@@ -1,22 +1,74 @@
 #[macro_use]
-extern crate intrusive_collections;
-use intrusive_collections::{LinkedList, LinkedListLink};
+extern crate lazy_static;
+extern crate regex;
+extern crate petgraph;
+extern crate itertools;
 
-struct Step {
-    part: &str,
-    link: LinkedListLink
+use petgraph::Direction::Incoming;
+use petgraph::graph::NodeIndex;
+use petgraph::graph::Graph;
+use regex::Regex;
+use std::collections::HashMap;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+lazy_static! {
+    static ref REQUIREMENT: Regex = Regex::new(r"^Step (\w+) must be finished before step (\w+) can begin.$").unwrap();
 }
 
-intrusive_adapter!(ValueAdapter<'a> = &'a Step: Step { link: LinkedListLink });
+fn parse_line(line: &str) -> Option<(&str, &str)> {
+    let captures = REQUIREMENT.captures(&line)?;
+    Some((captures.get(1)?.as_str(), captures.get(2)?.as_str()))
+}
 
-fn part1(input: &str) -> Option<String> {
-    let output = String::new();
+fn has_required(step_graph: &Graph<&str, &str>, node_id: NodeIndex, output: &str) -> bool {
+    let mut neighbor_requires: Vec<_> = step_graph.neighbors_directed(node_id, Incoming).collect();
+    let res = neighbor_requires.into_iter().all(|n| output.contains(step_graph[n]));
+    res
+}
+
+fn walk(step_graph: &Graph<&str, &str>, id: NodeIndex) -> Option<String> {
+    let mut neighbors: Vec<_> = step_graph.neighbors(id).collect();
+    let mut output = String::from(step_graph[id]);
+
+    while !neighbors.is_empty() {
+        let min = *neighbors
+            .iter()
+            .filter(|&&n| has_required(&step_graph, n, &output))
+            .min_by_key(|&&n| step_graph[n]).unwrap();
+        output += step_graph[min];
+
+        neighbors = neighbors.into_iter().filter(|&n| n != min).collect();
+        neighbors.extend(step_graph.neighbors(min));
+    }
 
     Some(output)
 }
 
+fn part1<'a>(input: &'a str) -> Option<String> {
+    let _requirements: Option<_> = input.lines().map(parse_line).collect();
+    let requirements: Vec<(&str, &str)> = _requirements?;
+
+    let mut step_graph = Graph::<&str, &str>::new();
+    let mut steps: HashMap<&str, NodeIndex> = HashMap::new();
+    for (before, after) in requirements.iter() {
+        steps.entry(before).or_insert_with(|| step_graph.add_node(before));
+        steps.entry(after).or_insert_with(|| step_graph.add_node(after));
+    }
+
+    step_graph.extend_with_edges(requirements.iter().map(|(before, after): &(&str, &str)| {
+        (steps[before], steps[after])
+    }));
+
+    let sources: Vec<NodeIndex> = step_graph.externals(Incoming).collect();
+    let root = *sources.last()?;
+
+    walk(&step_graph, root)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+    println!("{}", part1(input).unwrap());
 }
 
 #[test]
