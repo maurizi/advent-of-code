31d071fdba30ccd97dec278f4c04d1b9fa2fa7d2
Michael Maurizi <mmaurizi@azavea.com>
2018-12-09 12:05:24 -0500

    2018 Day 7 complete

--- aoc-2018-7/src/main.rs
+++ aoc-2018-7/src/main.rs
@@ -9,8 +9,8 @@ use petgraph::graph::NodeIndex;
 use petgraph::graph::Graph;
 use regex::Regex;
 use std::collections::HashMap;
+use petgraph::Directed;
 use itertools::Itertools;
-use std::collections::HashSet;
 
 lazy_static! {
     static ref REQUIREMENT: Regex = Regex::new(r"^Step (\w+) must be finished before step (\w+) can begin.$").unwrap();
@@ -22,53 +22,100 @@ fn parse_line(line: &str) -> Option<(&str, &str)> {
 }
 
 fn has_required(step_graph: &Graph<&str, &str>, node_id: NodeIndex, output: &str) -> bool {
-    let mut neighbor_requires: Vec<_> = step_graph.neighbors_directed(node_id, Incoming).collect();
+    let neighbor_requires: Vec<_> = step_graph.neighbors_directed(node_id, Incoming).collect();
     let res = neighbor_requires.into_iter().all(|n| output.contains(step_graph[n]));
     res
 }
 
-fn walk(step_graph: &Graph<&str, &str>, id: NodeIndex) -> Option<String> {
-    let mut neighbors: Vec<_> = step_graph.neighbors(id).collect();
-    let mut output = String::from(step_graph[id]);
+fn walk(step_graph: &Graph<&str, &str>) -> Option<String> {
+    let mut available: Vec<NodeIndex> = step_graph.externals(Incoming).collect();
+    let mut output = String::new();
 
-    while !neighbors.is_empty() {
-        let min = *neighbors
+    while !available.is_empty() {
+        let min = *available
             .iter()
             .filter(|&&n| has_required(&step_graph, n, &output))
             .min_by_key(|&&n| step_graph[n]).unwrap();
         output += step_graph[min];
 
-        neighbors = neighbors.into_iter().filter(|&n| n != min).collect();
-        neighbors.extend(step_graph.neighbors(min));
+        available = available.into_iter().filter(|&n| n != min).collect();
+        available.extend(step_graph.neighbors(min));
     }
 
     Some(output)
 }
 
-fn part1<'a>(input: &'a str) -> Option<String> {
+fn part1(input: &str) -> Option<String> {
+    let step_graph = get_step_graph(input)?;
+    walk(&step_graph)
+}
+
+fn get_step_graph(input: &str) -> Option<Graph<&str, &str>> {
     let _requirements: Option<_> = input.lines().map(parse_line).collect();
     let requirements: Vec<(&str, &str)> = _requirements?;
-
     let mut step_graph = Graph::<&str, &str>::new();
     let mut steps: HashMap<&str, NodeIndex> = HashMap::new();
     for (before, after) in requirements.iter() {
         steps.entry(before).or_insert_with(|| step_graph.add_node(before));
         steps.entry(after).or_insert_with(|| step_graph.add_node(after));
     }
-
     step_graph.extend_with_edges(requirements.iter().map(|(before, after): &(&str, &str)| {
         (steps[before], steps[after])
     }));
+    Some(step_graph)
+}
 
-    let sources: Vec<NodeIndex> = step_graph.externals(Incoming).collect();
-    let root = *sources.last()?;
+fn get_step_time(step: &str, delay: u8) -> Option<usize> {
+    Some(step.bytes().next()? as usize - 'A' as usize + 1 + delay as usize)
+}
 
-    walk(&step_graph, root)
+fn build_time(step_graph: &Graph<&str, &str>, workers: u8, delay: u8) -> Option<usize> {
+    let mut available: Vec<NodeIndex> = step_graph.externals(Incoming).collect();
+    let mut done = String::new();
+    let mut in_progress = vec![None; workers as usize];
+    let mut time = 0;
+
+    while !available.is_empty() || in_progress.iter().any(Option::is_some) {
+        let open_workers: Vec<_> = in_progress.iter().positions(Option::is_none).collect();
+        for worker in open_workers {
+            let next = available
+                .iter()
+                .cloned()
+                .filter(|&n| has_required(&step_graph, n, &done))
+                .min_by_key(|&n| step_graph[n]);
+            if let Some(next_step) = next {
+                in_progress[worker] = Some((next_step, time + get_step_time(step_graph[next_step], delay)?));
+                available = available.into_iter().filter(|&n| n != next_step).collect();
+            }
+        }
+        time += 1;
+        let complete: Vec<_> = in_progress.iter().positions(|&work| {
+            if let Some((step, end_time)) = work {
+                end_time == time
+            } else {
+                false
+            }
+        }).collect();
+        for index in complete {
+            let node = in_progress[index]?.0;
+            done += step_graph[node];
+            available.extend(step_graph.neighbors(node));
+            in_progress[index] = None;
+        }
+    }
+
+    Some(time)
+}
+
+fn part2(input: &str, workers: u8, delay: u8) -> Option<usize> {
+    let step_graph = get_step_graph(input)?;
+    build_time(&step_graph, workers, delay)
 }
 
 fn main() {
     let input = include_str!("input.txt");
     println!("{}", part1(input).unwrap());
+    println!("{}", part2(input, 5, 60).unwrap());
 }
 
 #[test]
@@ -82,4 +129,17 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.
 ";
     assert_eq!(part1(input), Some("CABDFE".to_owned()));
+}
+
+#[test]
+fn test_part2() {
+    let input = "Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+";
+    assert_eq!(part2(input, 2, 0), Some(15));
 }
