2a52a97ecdea33b3735a6280c3fbe6e08c600579
Michael Maurizi <mmaurizi@azavea.com>
2018-12-17 11:57:17 -0500

    2018 Day 13 part 2

--- aoc-2018-13/src/main.rs
+++ aoc-2018-13/src/main.rs
@@ -38,7 +38,7 @@ enum Turn {
     Straight, Left, Right
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct Cart {
     direction: Dir,
     next_turn: Turn,
@@ -50,16 +50,17 @@ impl Cart {
     }
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Clone)]
 enum Item {
     Track(Track),
-    TrackWithCart(Track, Cart)
+    TrackWithCrash(Track),
+    TrackWithCart(Track, Cart),
 }
 
 impl Item {
     fn remove_cart(self) -> (Track, Option<Cart>) {
         match self {
-            Item::Track(track) => (track, None),
+            Item::Track(track) | Item::TrackWithCrash(track) => (track, None),
             Item::TrackWithCart(track, cart) => {
                 (track, Some(cart))
             }
@@ -75,10 +76,11 @@ impl Debug for Item {
             Item::Track(Track::Vertical) => '|',
             Item::Track(Track::Horizontal) => '-',
             Item::Track(Track::Intersection) => '+',
-            Item::TrackWithCart(_, Cart{ direction: Dir::Down, next_turn }) => 'v',
-            Item::TrackWithCart(_, Cart{ direction: Dir::Up, next_turn }) => '^',
-            Item::TrackWithCart(_, Cart{ direction: Dir::Right, next_turn }) => '>',
-            Item::TrackWithCart(_, Cart{ direction: Dir::Left, next_turn }) => '<',
+            Item::TrackWithCart(_, Cart{ direction: Dir::Down, .. }) => 'v',
+            Item::TrackWithCart(_, Cart{ direction: Dir::Up, .. }) => '^',
+            Item::TrackWithCart(_, Cart{ direction: Dir::Right, .. }) => '>',
+            Item::TrackWithCart(_, Cart{ direction: Dir::Left, .. }) => '<',
+            Item::TrackWithCrash( .. ) => 'X',
         })
     }
 }
@@ -109,7 +111,7 @@ impl Debug for Tracks {
 
 fn parse(input: &str) -> Tracks {
     let mut carts = vec![];
-    let grid = input.trim().lines().enumerate().map(|(y, line)| {
+    let grid = input.lines().enumerate().map(|(y, line)| {
         line.bytes().enumerate().map(|(x, b)| {
             let (item, cart) = match b as char {
                 '/' => (Some(Item::Track(Track::ForwardCurve)), false),
@@ -132,11 +134,20 @@ fn parse(input: &str) -> Tracks {
     Tracks { grid, carts }
 }
 
-fn tick(tracks: &mut Tracks) -> Option<(usize, usize)> {
+fn tick(tracks: &mut Tracks) -> Vec<(usize, usize)> {
     let mut new_carts = Vec::with_capacity(tracks.carts.len());
+    let mut crashes = vec![];
+
     tracks.carts.sort();
     for (x, y) in tracks.carts.iter().cloned() {
+        // If we already crashed into the car, don't move it
+        if crashes.contains(&(x, y)) {
+            continue;
+        }
+
         let (cur_track, cart) = tracks.grid[y][x].take().unwrap().remove_cart();
+        tracks.grid[y][x] = Some(Item::Track(cur_track));
+
         let mut cart = cart.unwrap();
         let (new_x, new_y) = match cart.direction {
             Dir::Up => (x, y - 1),
@@ -144,10 +155,14 @@ fn tick(tracks: &mut Tracks) -> Option<(usize, usize)> {
             Dir::Left => (x - 1, y),
             Dir::Right => (x + 1, y),
         };
-        let next_item = tracks.grid[new_y][new_x].take().unwrap();
+
+        let next_item = tracks.grid[new_y][new_x].clone().unwrap();
 
         match next_item {
-            Item::TrackWithCart(_, _) => return Some((new_x, new_y)),
+            Item::TrackWithCart(next_track, ..) | Item::TrackWithCrash(next_track) => {
+                tracks.grid[new_y][new_x] = Some(Item::TrackWithCrash(next_track));
+                crashes.push((new_x, new_y));
+            },
             Item::Track(next_track) => {
                 cart.direction = match (next_track, cart.direction) {
                     (Track::Vertical, _) | (Track::Horizontal, _) => cart.direction,
@@ -170,42 +185,51 @@ fn tick(tracks: &mut Tracks) -> Option<(usize, usize)> {
                     }
                 };
                 tracks.grid[new_y][new_x] = Some(Item::TrackWithCart(next_track, cart));
-                tracks.grid[y][x] = Some(Item::Track(cur_track));
                 new_carts.push((new_x, new_y));
             },
         }
     }
-    tracks.carts = new_carts;
-    None
+    tracks.carts = new_carts.into_iter().filter(|coords| !crashes.contains(coords)).collect();
+    crashes
 }
 
-fn part1(input: &str) -> (usize, usize) {
-    let mut tracks = parse(input);
-
+fn part1(tracks: &mut Tracks) -> Vec<(usize, usize)> {
     loop {
-        if let Some(coordinates) = tick(&mut tracks) {
-            return coordinates
+        let coordinates = tick(tracks);
+        if coordinates.len() > 0 {
+            return coordinates;
         }
     }
 }
 
+fn part2(tracks: &mut Tracks) -> (usize, usize) {
+    while tracks.carts.len() > 1 {
+        let crashes = part1(tracks);
+        for (x, y) in crashes {
+            let (track, _) = tracks.grid[y][x].take().unwrap().remove_cart();
+            tracks.grid[y][x] = Some(Item::Track(track));
+        }
+    }
+    tracks.carts[0]
+}
+
 fn main() {
-    let input = include_str!("input.txt");
-    println!("{:?}", part1(input));
+    let mut tracks = parse(include_str!("input.txt"));
+    println!("part 1 {:?}", part1(&mut tracks));
+    let mut tracks = parse(include_str!("input.txt"));
+    println!("part 2 {:?}", part2(&mut tracks));
 }
 
 #[test]
 fn test_tick() {
-    let input = r#"
-/->-\
+    let input = r#"/->-\
 |   |  /----\
 | /-+--+-\  |
 | | |  | v  |
 \-+-/  \-+--/
   \------/"#;
 
-    let output_3 = r#"
-/---\
+    let output_3 = r#"/---\
 |   v  /----\
 | /-+--+-\  |
 | | |  | |  |
@@ -225,13 +249,25 @@ fn test_tick() {
 
 #[test]
 fn test_part1() {
-    let input = r#"
-/->-\
+    let input = r#"/->-\
 |   |  /----\
 | /-+--+-\  |
 | | |  | v  |
 \-+-/  \-+--/
   \------/"#;
 
-    assert_eq!(part1(input), (7, 3))
+    assert_eq!(part1(&mut parse(input)), vec![(7, 3)])
+}
+
+#[test]
+fn test_part2() {
+    let input = r#"/>-<\
+|   |
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/"#;
+
+    assert_eq!(part2(&mut parse(input)), (6, 4))
 }
